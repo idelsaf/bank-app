b5ce8d4b62e83a9be21b4fe6b5cd8d679961b8bd
idel <little1student@gmail.com>
2024-05-08 19:33:43 +0300

    refactor: clean Main

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -1,26 +1,10 @@
-import db_objs.User;
-import guis.BankAppGui;
 import guis.LoginGui;
-import guis.RegisterGui;
-
-import javax.swing.*;
-import java.math.BigDecimal;
+import javax.swing.SwingUtilities;
 
 public class AppLauncher {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             new LoginGui().setVisible(true);
-//                new RegisterGui().setVisible(true);
-//            new BankAppGui(
-//                    new User(1,
-//                            "bipbup",
-//                            "1111",
-//                            "Gay",
-//                            "89988987654",
-//                            BigDecimal.valueOf(1000),
-//                            "3453453453452",
-//                            "1323")
-//            ).setVisible(true);
         });
     }
 }
